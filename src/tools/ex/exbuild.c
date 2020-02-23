9576a97e05066647e762f6b48d90148ef47e0f31
Nunuhara Cabbage <nunuhara@haniwa.technology>
2020-02-22 23:06:32 -0800

    exbuild: write (binary) ex file to disk
    It is now possible to modify .ex files using exdump and exbuild. E.g.
    
        exdump -o out.x Rance10EX.ex
        $EDITOR out.x
        exbuild -o Rance10EX.ex out.x

--- src/tools/ex/exbuild.c
+++ src/tools/ex/exbuild.c
@@ -15,20 +15,85 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <getopt.h>
 #include "system4.h"
 #include "system4/ex.h"
 #include "system4/string.h"
 #include "system4/utfsjis.h"
 #include "ast.h"
 
-int ex_dump(FILE *out, struct ex *ex);
+void ex_write(FILE *out, struct ex *ex);
+
+static void usage(void)
+{
+	puts("Usage: exbuild [options...] input-file");
+	puts("    Build EX files.");
+	puts("");
+	puts("    -h, --help      Display this message and exit");
+	puts("    -o, --output    Set the output file path");
+}
+
+enum {
+	LOPT_HELP = 256,
+	LOPT_OUTPUT,
+};
 
 int main(int argc, char *argv[])
 {
-	struct ex *ex = ex_parse(stdin);
-	ex_dump(stdout, ex);
-	fflush(stdout);
-	ex_free(ex);
+	const char *output_file = NULL;
 
+	while (1) {
+		static struct option long_options[] = {
+			{ "help",   no_argument,       0, LOPT_HELP },
+			{ "output", required_argument, 0, LOPT_OUTPUT },
+		};
+		int option_index = 0;
+		int c;
+
+		c = getopt_long(argc, argv, "ho:", long_options, &option_index);
+		if (c == -1)
+			break;
+
+		switch (c) {
+		case 'h':
+		case LOPT_HELP:
+			usage();
+			return 0;
+		case 'o':
+		case LOPT_OUTPUT:
+			output_file = optarg;
+			break;
+		case '?':
+			ERROR("Unknown command line argument");
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 1) {
+		usage();
+		ERROR("Wrong number of arguments.");
+	}
+
+	FILE *out = stdout;
+	if (output_file)
+		out = fopen(output_file, "wb");
+	if (!out)
+		ERROR("fopen failed: %s", strerror(errno));
+
+	FILE *in;
+	if (!strcmp(argv[0], "-"))
+		in = stdin;
+	else
+		in = fopen(argv[0], "rb");
+	if (!in)
+		ERROR("fopen failed: %s", strerror(errno));
+
+	struct ex *ex = ex_parse(in);
+	ex_write(out, ex);
+	ex_free(ex);
 	return 0;
 }
