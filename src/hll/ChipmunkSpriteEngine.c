ced0a832cca392d20417ffc7f3db269e74666e8f
Nunuhara Cabbage <nunuhara@haniwa.technology>
2022-06-16 09:47:26 -0700

    Implement ChipmunkSpriteEngine.Sleep

--- src/hll/ChipmunkSpriteEngine.c
+++ src/hll/ChipmunkSpriteEngine.c
@@ -21,6 +21,7 @@
 #include "system4/utfsjis.h"
 
 #include "asset_manager.h"
+#include "gfx/gfx.h"
 #include "gfx/font.h"
 #include "vm/page.h"
 #include "input.h"
@@ -79,7 +80,11 @@ static bool keep_previous_view = true;
 HLL_WARN_UNIMPLEMENTED((keep_previous_view = on, true), bool, ChipmunkSpriteEngine, KeepPreviousView_SetMode, bool on);
 HLL_WARN_UNIMPLEMENTED(keep_previous_view, bool, ChipmunkSpriteEngine, KeepPreviousView_GetMode);
 HLL_QUIET_UNIMPLEMENTED(false, bool, ChipmunkSpriteEngine, KeepPreviousView);
-HLL_QUIET_UNIMPLEMENTED(, void, ChipmunkSpriteEngine, Sleep);
+
+static void ChipmunkSpriteEngine_Sleep(void)
+{
+	SDL_Delay(16);
+}
 
 //static bool ChipmunkSpriteEngine_SP_SetCutCG(int sp_no, int cg_no, int cut_x, int cut_y, int cut_w, int cut_h);
 //static bool ChipmunkSpriteEngine_SP_SetMultipleColor(int sp_no, int r, int g, int b);
