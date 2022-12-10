9a6553f2155f0b46dc9076f2acdf6e912a440336
kichikuou <KichikuouChrome@gmail.com>
2022-12-10 17:37:26 +0900

    ReignEngine: Fix glTexImage2D arguments for shadow renderer
    GL_DEPTH_COMPONENT is not a valid value for internalFormat in OpenGL ES.

--- src/3d/renderer.c
+++ src/3d/renderer.c
@@ -104,7 +104,7 @@ static void init_shadow_renderer(struct shadow_renderer *sr)
 	glGenFramebuffers(1, &sr->fbo);
 	glGenTextures(1, &sr->texture);
 	glBindTexture(GL_TEXTURE_2D, sr->texture);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT16, SHADOW_WIDTH, SHADOW_HEIGHT, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_SHORT, NULL);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
