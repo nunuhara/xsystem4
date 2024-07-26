da86fd4875ba6bcea1dd4051b27261896091f98e
kichikuou <KichikuouChrome@gmail.com>
2024-07-27 07:55:32 +0900

    vmGraph changes for DALK Gaiden
    The effect API is slightly changed so that it can be used from pre-SACT
    graphics engines.

--- src/hll/vmGraph.c
+++ src/hll/vmGraph.c
@@ -14,6 +14,7 @@
  * along with this program; if not, see <http://gnu.org/licenses/>.
  */
 
+#include "effect.h"
 #include "gfx/gfx.h"
 #include "vmSurface.h"
 #include "hll.h"
@@ -61,7 +62,15 @@ static void vmGraph_CopySprite(int dst_surface, int dx, int dy, int src_surface,
 
 //void vmGraph_Blend(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height, int alpha);
 //void vmGraph_BlendSrcBright(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height, int alpha, int nRate);
-//void vmGraph_BlendAddSatur(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height);
+
+static void vmGraph_BlendAddSatur(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height)
+{
+	struct vm_surface *dst = vm_surface_get(dst_surface);
+	struct vm_surface *src = vm_surface_get(src_surface);
+	if (!dst || !src)
+		return;
+	gfx_blend_add_satur(&dst->texture, dx, dy, &src->texture, sx, sy, width, height);
+}
 
 static void vmGraph_BlendAlphaMap(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height)
 {
@@ -79,7 +88,16 @@ static void vmGraph_BlendAlphaMap(int dst_surface, int dx, int dy, int src_surfa
 //void vmGraph_BlendAlphaMapBright(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height, int nRate);
 //void vmGraph_BlendAlphaMapAlphaSrcBright(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height, int alpha, int nRate);
 //void vmGraph_BlendUseAMapColor(int dst_surface, int dx, int dy, int width, int height, int hAlphaSurface, int nAx, int nAy, int r, int g, int b, int alpha);
-//void vmGraph_BlendScreen(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height);
+
+static void vmGraph_BlendScreen(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height)
+{
+	struct vm_surface *dst = vm_surface_get(dst_surface);
+	struct vm_surface *src = vm_surface_get(src_surface);
+	if (!dst || !src)
+		return;
+	gfx_blend_screen(&dst->texture, dx, dy, &src->texture, sx, sy, width, height);
+}
+
 //void vmGraph_BlendMultiply(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height);
 //void vmGraph_BlendScreenAlpha(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height, int alpha);
 
@@ -127,7 +145,15 @@ static void vmGraph_AddDA_DAxSA(int dst_surface, int dx, int dy, int src_surface
 	gfx_add_da_daxsa(&dst->texture, dx, dy, &src->texture, sx, sy, width, height);
 }
 
-//void vmGraph_SubDA_DAxSA(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height);
+static void vmGraph_SubDA_DAxSA(int dst_surface, int dx, int dy, int src_surface, int sx, int sy, int width, int height)
+{
+	struct vm_surface *dst = vm_surface_get(dst_surface);
+	struct vm_surface *src = vm_surface_get(src_surface);
+	if (!dst || !src)
+		return;
+	gfx_sub_da_daxsa(&dst->texture, dx, dy, &src->texture, sx, sy, width, height);
+}
+
 //void vmGraph_BrightDestOnly(int surface, int x, int y, int width, int height, int nRate);
 //void vmGraph_CopyTextureWrap(int dst_surface, int dx, int dy, int d_width, int d_height, int src_surface, int sx, int sy, int s_width, int s_height, int nU, int nV);
 //void vmGraph_CopyTextureWrapAlpha(int dst_surface, int dx, int dy, int d_width, int d_height, int src_surface, int sx, int sy, int s_width, int s_height, int nU, int nV, int alpha);
@@ -158,7 +184,33 @@ static void vmGraph_CopyStretch(int surface, int dx, int dy, int d_width, int d_
 //void vmGraph_CopyStretchBlend(int surface, int dx, int dy, int d_width, int d_height, int src_surface, float sx, float sy, float s_width, float s_height, int alpha);
 //void vmGraph_CopyStretchBlendAlphaMap(int surface, int dx, int dy, int d_width, int d_height, int src_surface, float sx, float sy, float s_width, float s_height);
 HLL_WARN_UNIMPLEMENTED( , void, vmGraph, SetClickCancel, int click);
-//int vmGraph_EffectCopy(int dx, int dy, int src_surface, int sx, int sy, int width, int height, int nEffect, int nTotalTime);
+
+static int vmGraph_EffectCopy(int dx, int dy, int src_surface, int sx, int sy, int width, int height, int effect, int total_time)
+{
+	uint32_t start = SDL_GetTicks();
+	if (!effect_init(effect))
+		return 0;
+	effect_record_old();
+	vmGraph_Copy(vm_surface_get_main_surface(), dx, dy, src_surface, sx, sy, width, height);
+	effect_record_new();
+
+	uint32_t t = SDL_GetTicks() - start;
+	while (t < total_time) {
+		effect_update((float)t / (float)total_time);
+		uint32_t t2 = SDL_GetTicks() - start;
+		if (t2 < t + 16) {
+			SDL_Delay(t + 16 - t2);
+			t += 16;
+		} else {
+			t = t2;
+		}
+	}
+	effect_update(1.0);
+
+	effect_fini();
+	return 1;
+}
+
 //int vmGraph_GetMainWidth(void);
 //int vmGraph_GetMainHeight(void);
 //int vmGraph_GetMainBpp(void);
@@ -178,7 +230,7 @@ HLL_LIBRARY(vmGraph,
 	    HLL_EXPORT(CopySprite, vmGraph_CopySprite),
 	    HLL_TODO_EXPORT(Blend, vmGraph_Blend),
 	    HLL_TODO_EXPORT(BlendSrcBright, vmGraph_BlendSrcBright),
-	    HLL_TODO_EXPORT(BlendAddSatur, vmGraph_BlendAddSatur),
+	    HLL_EXPORT(BlendAddSatur, vmGraph_BlendAddSatur),
 	    HLL_EXPORT(BlendAlphaMap, vmGraph_BlendAlphaMap),
 	    HLL_TODO_EXPORT(BlendAlphaMapSrcOnly, vmGraph_BlendAlphaMapSrcOnly),
 	    HLL_TODO_EXPORT(BlendAlphaMapColor, vmGraph_BlendAlphaMapColor),
@@ -187,7 +239,7 @@ HLL_LIBRARY(vmGraph,
 	    HLL_TODO_EXPORT(BlendAlphaMapBright, vmGraph_BlendAlphaMapBright),
 	    HLL_TODO_EXPORT(BlendAlphaMapAlphaSrcBright, vmGraph_BlendAlphaMapAlphaSrcBright),
 	    HLL_TODO_EXPORT(BlendUseAMapColor, vmGraph_BlendUseAMapColor),
-	    HLL_TODO_EXPORT(BlendScreen, vmGraph_BlendScreen),
+	    HLL_EXPORT(BlendScreen, vmGraph_BlendScreen),
 	    HLL_TODO_EXPORT(BlendMultiply, vmGraph_BlendMultiply),
 	    HLL_TODO_EXPORT(BlendScreenAlpha, vmGraph_BlendScreenAlpha),
 	    HLL_EXPORT(Fill, vmGraph_Fill),
@@ -197,7 +249,7 @@ HLL_LIBRARY(vmGraph,
 	    HLL_TODO_EXPORT(SaturDP_DPxSA, vmGraph_SaturDP_DPxSA),
 	    HLL_TODO_EXPORT(ScreenDA_DAxSA, vmGraph_ScreenDA_DAxSA),
 	    HLL_EXPORT(AddDA_DAxSA, vmGraph_AddDA_DAxSA),
-	    HLL_TODO_EXPORT(SubDA_DAxSA, vmGraph_SubDA_DAxSA),
+	    HLL_EXPORT(SubDA_DAxSA, vmGraph_SubDA_DAxSA),
 	    HLL_TODO_EXPORT(BrightDestOnly, vmGraph_BrightDestOnly),
 	    HLL_TODO_EXPORT(CopyTextureWrap, vmGraph_CopyTextureWrap),
 	    HLL_TODO_EXPORT(CopyTextureWrapAlpha, vmGraph_CopyTextureWrapAlpha),
@@ -205,7 +257,7 @@ HLL_LIBRARY(vmGraph,
 	    HLL_TODO_EXPORT(CopyStretchBlend, vmGraph_CopyStretchBlend),
 	    HLL_TODO_EXPORT(CopyStretchBlendAlphaMap, vmGraph_CopyStretchBlendAlphaMap),
 	    HLL_EXPORT(SetClickCancel, vmGraph_SetClickCancel),
-	    HLL_TODO_EXPORT(EffectCopy, vmGraph_EffectCopy),
+	    HLL_EXPORT(EffectCopy, vmGraph_EffectCopy),
 	    HLL_EXPORT(GetMainSurface, vm_surface_get_main_surface),
 	    HLL_TODO_EXPORT(GetMainWidth, vmGraph_GetMainWidth),
 	    HLL_TODO_EXPORT(GetMainHeight, vmGraph_GetMainHeight),
