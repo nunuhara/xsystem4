nunuhara/xsystem4

commit ed2239fb1b1beaaabb67781867ce43f687bf1f8b

Use xcalloc_aligned for allocating structures containing mat4
This fixes #294.

cglm's mat4 requires 32-byte alignment when __AVX__ is defined.

src/3d/model.c
@@ -518,7 +518,7 @@ struct model *model_load(struct archive *aar, const char *path)
 		model->bone_map = ht_create(pol->nr_bones * 3 / 2);
 		model->bone_name_map = ht_create(pol->nr_bones * 3 / 2);
 		model->mot_cache = ht_create(16);
-		model->bones = xcalloc(pol->nr_bones, sizeof(struct bone));
+		model->bones = xcalloc_aligned(pol->nr_bones, struct bone);
 		for (uint32_t i = 0; i < pol->nr_bones; i++) {
 			add_bone(model, pol, &pol->bones[i]);
 		}

src/3d/reign.c
@@ -38,7 +38,7 @@ enum RE_plugin_version re_plugin_version;
 
 static struct RE_instance *create_instance(struct RE_plugin *plugin)
 {
-	struct RE_instance *instance = xcalloc(1, sizeof(struct RE_instance));
+	struct RE_instance *instance = xcalloc_aligned(1, struct RE_instance);
 	instance->plugin = plugin;
 	for (int i = 0; i < RE_NR_INSTANCE_TARGETS; i++)
 		instance->target[i] = -1;
@@ -229,7 +229,7 @@ struct RE_plugin *RE_plugin_new(enum RE_plugin_version version)
 	if (!aar)
 		return NULL;
 
-	struct RE_plugin *plugin = xcalloc(1, sizeof(struct RE_plugin));
+	struct RE_plugin *plugin = xcalloc_aligned(1, struct RE_plugin);
 	plugin->plugin.name = "ReignEngine";
 	plugin->plugin.update = RE_render;
 	plugin->plugin.to_json = RE_to_json;

src/dungeon/dungeon.c
@@ -60,7 +60,7 @@ struct dungeon_context *dungeon_get_context(int surface)
 
 struct dungeon_context *dungeon_context_create(enum draw_dungeon_version version, int width, int height)
 {
-	struct dungeon_context *ctx = xcalloc(1, sizeof(struct dungeon_context));
+	struct dungeon_context *ctx = xcalloc_aligned(1, struct dungeon_context);
 	ctx->plugin.name = plugin_name;
 	ctx->plugin.free = dungeon_context_free;
 	ctx->plugin.update = dungeon_render;

src/parts/flash.c
@@ -153,7 +153,7 @@ static struct parts_flash_object *update_object(struct parts_flash_object *obj, 
 {
 	if (!obj) {
 		if (tag->flags & PLACE_FLAG_HAS_CHARACTER) {
-			obj = xcalloc(1, sizeof(struct parts_flash_object));
+			obj = xcalloc_aligned(1, struct parts_flash_object);
 			obj->depth = tag->depth;
 		} else {
 			ERROR("no object to update (depth=%d)", tag->depth);

commit 5f13b3e9d0ee73e5e719ce30324eae0d255cdae2

Fix crash on Windows introduced in commit ed2239f
Memory allocated with xcalloc_aligned (which uses _aligned_malloc on
Windows) must be freed with _aligned_free. Using standard free() caused
heap corruption and crashes.

src/3d/model.c
@@ -594,7 +594,7 @@ void model_free(struct model *model)
 
 	for (int i = 0; i < model->nr_bones; i++)
 		destroy_bone(&model->bones[i]);
-	free(model->bones);
+	xfree_aligned(model->bones);
 	if (model->bone_map)
 		ht_free_int(model->bone_map);
 	if (model->bone_name_map)

src/3d/reign.c
@@ -94,7 +94,7 @@ static void unload_instance(struct RE_instance *instance)
 static void free_instance(struct RE_instance *instance)
 {
 	unload_instance(instance);
-	free(instance);
+	xfree_aligned(instance);
 }
 
 static void RE_back_cg_init(struct RE_back_cg *bcg)
@@ -263,7 +263,7 @@ void RE_plugin_free(struct RE_plugin *plugin)
 		RE_renderer_free(plugin->renderer);
 	for (int i = 0; i < RE_NR_BACK_CGS; i++)
 		RE_back_cg_destroy(&plugin->back_cg[i]);
-	free(plugin);
+	xfree_aligned(plugin);
 }
 
 bool RE_plugin_bind(struct RE_plugin *plugin, int sprite)

src/dungeon/dungeon.c
@@ -102,7 +102,7 @@ static void dungeon_context_free(struct draw_plugin *plugin)
 	if (ctx->map)
 		dungeon_map_free(ctx->map);
 
-	free(ctx);
+	xfree_aligned(ctx);
 }
 
 static GLuint *load_event_textures(int *nr_textures_out)

src/parts/flash.c
@@ -38,7 +38,7 @@ static void clear_display_list(struct parts_flash *f)
 	while (!TAILQ_EMPTY(&f->display_list)) {
 		struct parts_flash_object *obj = TAILQ_FIRST(&f->display_list);
 		TAILQ_REMOVE(&f->display_list, obj, entry);
-		free(obj);
+		xfree_aligned(obj);
 	}
 }
 
@@ -62,7 +62,7 @@ void parts_flash_free(struct parts_flash *f)
 		ht_free_int(f->bitmaps);
 	}
 	if (f->sprites) {
-		ht_foreach_value(f->sprites, free);
+		ht_foreach_value(f->sprites, xfree_aligned);
 		ht_free_int(f->sprites);
 	}
 }
@@ -216,7 +216,7 @@ static void remove_object(struct parts_flash *f, struct swf_tag_remove_object2 *
 	TAILQ_FOREACH(p, &f->display_list, entry) {
 		if (p->depth == tag->depth) {
 			TAILQ_REMOVE(&f->display_list, p, entry);
-			free(p);
+			xfree_aligned(p);
 			return;
 		}
 	}